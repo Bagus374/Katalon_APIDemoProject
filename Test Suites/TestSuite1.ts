f3af29ae7f77bfbf6ca7cdd5597c565d7ccc4c7a
bagusardianto374@gmail.com <bagusardianto374@gmail.com>
2020-10-06 00:40:46 +0700

    First project commit katalon

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>613ca652-3f5f-4a8c-8558-3dceb3e41339</testSuiteGuid>
+   <testCaseLink>
+      <guid>2ca733c7-a946-4871-a2f6-b5e6b7b6fbc4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>63f186da-4ad3-43ee-8c47-a8cd8e69db40</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f14b122d-f26b-4dd3-b498-efda4539eda2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/CalculatorTests/Addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>482b90ec-a556-4c9b-a86d-004f080bdde6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
